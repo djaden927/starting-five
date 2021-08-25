d2eff20bf24cfe4487af14b7404bf67e17addd76
djaden927 <djaden92@gmail.com>
2021-08-25 14:42:42 -0600

    all team data added. can build a team of 5.

--- src/components/TeamBuilder.js
+++ src/components/TeamBuilder.js
@@ -2,30 +2,73 @@ import {data} from "../data/lineupdata"
 import React, {useState} from "react"
 import styled from "styled-components"
 
+//This page handles generating 5 random teams, adding those players to your team and then submitting the team to the database.
+//Need to restrict adding positions that are already on the team.
+//need to be able to submit the team once all 5 positions are filled. 
+
+const initialStartingFive = {
+    PG: "",
+    SG: "",
+    SF: "",
+    PF: "",
+    C: ""
+}
+
+const MainTeamBuilderBox = styled.div`
+    display: flex;
+`
+
+const TeamSelector = styled.div`
+    margin: 0 5%;
+    border: 1px solid blue;
+`
+let count = 0
 export default function TeamBuilder(){
     const [fiveTeams, setFiveTeams] = useState([])
+    const [myStartingFive, setMyStartingFive] = useState(initialStartingFive)
+    
 
     const newTeam = () => {
         let randomTeamList = []
         for(let i = 0; i < 5; i++){
-            randomTeamList.push(data[Math.floor(Math.random() * 6)])
+            randomTeamList.push(data[Math.floor(Math.random() * 30)])
         }
         setFiveTeams(randomTeamList)
     }
+
+    const addPlayer = (e) => {
+        setMyStartingFive({...myStartingFive,
+            [e.target.id]: e.target.value
+        })
+        count += 1
+        console.log(count)
+    }
+
 
     return(
 
         <div>
             <h1>teambuilder page</h1>
             <button onClick={newTeam}>build a new team</button>
-            {fiveTeams.length > 0 ? 
-            <div>
-            <h2>{fiveTeams[0].PG}</h2>   <button onClick="addPlayer" id="PG">Pick {fiveTeams[0].PG}</button>
-            <h2>{fiveTeams[0].SG}</h2>  <button onClick="addPlayer" id="SG">Pick {fiveTeams[0].SG}</button>
-            <h2>{fiveTeams[0].SF}</h2>  <button onClick="addPlayer" id="SF">Pick {fiveTeams[0].SF}</button>
-            <h2>{fiveTeams[0].PF}</h2>  <button onClick="addPlayer" id="PF">Pick {fiveTeams[0].PF}</button>
-            <h2>{fiveTeams[0].C}</h2>  <button onClick="addPlayer" id="C">Pick {fiveTeams[0].C}</button>
-            </div> : null}
+            <MainTeamBuilderBox>
+                {fiveTeams.length > 0 && count < 5 ? 
+                <TeamSelector>
+                <h2>{fiveTeams[count].PG}</h2>   <button onClick={addPlayer} value={fiveTeams[count].PG} id="PG">Pick {fiveTeams[count].PG}</button>
+                <h2>{fiveTeams[count].SG}</h2>  <button onClick={addPlayer} value={fiveTeams[count].SG} id="SG">Pick {fiveTeams[count].SG}</button>
+                <h2>{fiveTeams[count].SF}</h2>  <button onClick={addPlayer} value={fiveTeams[count].SF} id="SF">Pick {fiveTeams[count].SF}</button>
+                <h2>{fiveTeams[count].PF}</h2>  <button onClick={addPlayer} value={fiveTeams[count].PF} id="PF">Pick {fiveTeams[count].PF}</button>
+                <h2>{fiveTeams[count].C}</h2>  <button onClick={addPlayer} value={fiveTeams[count].C} id="C">Pick {fiveTeams[count].C}</button>
+                </TeamSelector> : null}
+
+                <div>
+                    <h2>My starting five</h2>
+                    <h3>PG: {myStartingFive.PG}</h3>
+                    <h3>SG: {myStartingFive.SG}</h3>
+                    <h3>SF: {myStartingFive.SF}</h3>
+                    <h3>PF: {myStartingFive.PF}</h3>
+                    <h3>C: {myStartingFive.C}</h3>
+                </div>
+            </MainTeamBuilderBox>
         </div>
     )
 }
--- src/data/lineupdata.js
+++ src/data/lineupdata.js
@@ -46,5 +46,197 @@ export const data = [
         "SF": "Jaylen Brown",
         "PF": "Jayson Tatum",
         "C": "Robert Williams"
+    },
+    {
+        "team": "Hawks",
+        "PG": "Trae Young",
+        "SG": "Kevin Huerter",
+        "SF": "Bogdan Bogdanovic",
+        "PF": "John Collins",
+        "C": "Clint Capela"
+    },
+    {
+        "team": "Nets",
+        "PG": "Kyrie Irving",
+        "SG": "James Harden",
+        "SF": "Joe Harris",
+        "PF": "Kevin Durant",
+        "C": "Blake Griffin"
+    },
+    {
+        "team": "Hornets",
+        "PG": "LaMelo Ball",
+        "SG": "Terry Rozier",
+        "SF": "Gordon Hayward",
+        "PF": "P.J. Washington",
+        "C": "Mason Plumlee"
+    },
+    {
+        "team": "Bulls",
+        "PG": "Lonzo Bull",
+        "SG": "Zach LaVine",
+        "SF": "DeMar DeRozan",
+        "PF": "Lauri Markkanen",
+        "C": "Nikola Vucevic"
+    },
+    {
+        "team": "Cavs",
+        "PG": "Darius Garland",
+        "SG": "Collin Sexton",
+        "SF": "Isaac Okoro",
+        "PF": "Kevin Love",
+        "C": "Jarrett Allen"
+    },
+    {
+        "team": "Mavericks",
+        "PG": "Luka Doncic",
+        "SG": "Tim Hardaway Jr.",
+        "SF": "Dorian Finney-Smith",
+        "PF": "Maxi Kleber",
+        "C": "Kristaps Porzingis"
+    },
+    {
+        "team": "Pistons",
+        "PG": "Killian Hayes",
+        "SG": "Cade Cunningham",
+        "SF": "Saddiq Bey",
+        "PF": "Jerami Grant",
+        "C": "Isaiah Stewart"
+    },
+    {
+        "team": "Warriors",
+        "PG": "Stephen Curry",
+        "SG": "Klay Thompson",
+        "SF": "Andrew Wiggins",
+        "PF": "Draymond Green",
+        "C": "James Wiseman"
+    },
+    {
+        "team": "Rockets",
+        "PG": "John Wall",
+        "SG": "Kevin Porter Jr.",
+        "SF": "Jalen Green",
+        "PF": "Daniel Theis",
+        "C": "Christian Wood"
+    },
+    {
+        "team": "Pacers",
+        "PG": "Malcolm Brogdon",
+        "SG": "Caris LeVert",
+        "SF": "T.J. Warren",
+        "PF": "Domantas Sabonis",
+        "C": "Myles Turner"
+    },
+    {
+        "team": "Clippers",
+        "PG": "Reggie Jackson",
+        "SG": "Paul George",
+        "SF": "Nicolas Batum",
+        "PF": "Marcus Morris",
+        "C": "Ivica Zubac"
+    },
+    {
+        "team": "Grizzlies",
+        "PG": "Ja Morant",
+        "SG": "Dillon Brooks",
+        "SF": "Kyle Anderson",
+        "PF": "Jaren Jackson Jr.",
+        "C": "Steven Adams"
+    },
+    {
+        "team": "Heat",
+        "PG": "Kyle Lowry",
+        "SG": "Duncan Robinson",
+        "SF": "Jimmy Butler",
+        "PF": "P.J. Tucker",
+        "C": "Bam Adebayo"
+    },
+    {
+        "team": "Timberwolves",
+        "PG": "D'Angelo Russell",
+        "SG": "Anthony Edwards",
+        "SF": "Taurean Prince",
+        "PF": "Jaden McDaniels",
+        "C": "Karl-Anthony Towns"
+    },
+    {
+        "team": "Pelicans",
+        "PG": "Nickeil Alexander-Walker",
+        "SG": "Devonte Graham",
+        "SF": "Brandon Ingram",
+        "PF": "Zion Williamson",
+        "C": "Jonas Valanciunas"
+    },
+    {
+        "team": "Knicks",
+        "PG": "Kemba Walker",
+        "SG": "Evan Fournier",
+        "SF": "RJ Barrett",
+        "PF": "Julius Randle",
+        "C": "Mitchell Robinson"
+    },
+    {
+        "team": "Thunder",
+        "PG": "Shai Gilgeous-Alexander",
+        "SG": "Lu Dort",
+        "SF": "Aleksej Pokusevski",
+        "PF": "Darius Bazley",
+        "C": "Isaiah Roby"
+    },
+    {
+        "team": "Magic",
+        "PG": "Jalen Suggs",
+        "SG": "Gary Harris",
+        "SF": "Terrence Ross",
+        "PF": "James Ennis",
+        "C": "Wendell Carter Jr."
+    },
+    {
+        "team": "Suns",
+        "PG": "Chris Paul",
+        "SG": "Devin Booker",
+        "SF": "Miles Bridges",
+        "PF": "Jae Crowder",
+        "C": "Deandre Ayton"
+    },
+    {
+        "team": "Trail Blazers",
+        "PG": "Damian Lillard",
+        "SG": "C.J. McCollum",
+        "SF": "Norman Powell",
+        "PF": "Robert Covington",
+        "C": "Jusuf Nurkic"
+    },
+    {
+        "team": "Kings",
+        "PG": "De'Aaron Fox",
+        "SG": "Buddy Hield",
+        "SF": "Harrison Barnes",
+        "PF": "Marvin Bagley",
+        "C": "Richaun Holmes"
+    },
+    {
+        "team": "Spurs",
+        "PG": "Dejounte Murray",
+        "SG": "Derrick White",
+        "SF": "Keldon Johnson",
+        "PF": "Thaddeus Young",
+        "C": "Jakob Poeltl"
+    },
+    {
+        "team": "Raptors",
+        "PG": "Fred VanVleet",
+        "SG": "Gary Trent Jr.",
+        "SF": "OG Anunoby",
+        "PF": "Pascal Siakam",
+        "C": "Khem Birch"
+    },
+    {
+        "team": "Wizards",
+        "PG": "Spencer Dinwiddie",
+        "SG": "Bradley Beal",
+        "SF": "Kyle Kuzma",
+        "PF": "Rui Hachimura",
+        "C": "Thomas Bryant"
     }
 ]
